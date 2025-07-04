803890765002c7cd385d4700163fc39a54082af2
y20-code <yls3235882107@gmail.com>
2025-07-04 14:00:26 +0800

    feat:Initial project structure for backend and frontend

---
+++ backend/src/main/java/com/yls/vocabverse/VocabverseApplication.java
@@ -0,0 +1,13 @@
+package com.yls.vocabverse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocabverseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VocabverseApplication.class, args);
+	}
+
+}
